494df419eca90fee04d5253f4caf30ea6dd274b1
Divyesh Bakaraniya <bakaraniyadivyesh94@gmail.com>
2025-06-04 16:35:17 +0530

    some more fixes

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -13,8 +13,8 @@ import {
   RiFlowerFill,
   RiDashboardLine,
 } from "@remixicon/react";
-import { useAuthStore } from "../../store/useAuthStore";
-import useWishlistStore from "../../store/useWishlistStore";
+import { useAuthStore } from "@store/useAuthStore";
+import useWishlistStore from "@store/useWishlistStore";
 
 // Styled Badge component
 const StyledBadge = styled(Badge)(({ theme }) => ({
