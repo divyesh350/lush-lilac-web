438673edcd3b2973264cde9eacf4ac91fa3b9524
Divyesh Bakaraniya <bakaraniyadivyesh94@gmail.com>
2025-06-06 11:18:31 +0530

    admin vite config update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,5 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // important for Vercel
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['@mui/material', '@emotion/react', '@emotion/styled']
+  }
 })
